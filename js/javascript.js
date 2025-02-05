76065f9176c8a3b0fdbd8a7c7caf3115c729999c
rakal-8 <fntnlzk707@gmail.com>
2025-02-05 18:22:23 +0900

    js 파일 연결, 반응형 수정

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".slide", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
+});
