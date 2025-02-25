5030c788a012e4a497670df6f22773ced0c55515
RKL <fntnlzk707@gmail.com>
2025-02-25 21:46:05 +0900

    JS 코드 수정

--- js/javascript.js
+++ js/javascript.js
@@ -329,3 +329,20 @@ slider4.addEventListener("mousemove", (e) => {
 
 //--------------------------------------------------//
 //서브페이지 js//
+
+let swiper5 = new Swiper2(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
