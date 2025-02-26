8f6058a50ad2b4781e0033dd5f383097697f7a4d
rakal-8 <fntnlzk707@gmail.com>
2025-02-26 18:22:42 +0900

    SUB-커뮤니티 페이지 HTML, CSS, JS완료.

--- js/sub_javascript.js
+++ js/sub_javascript.js
@@ -2,7 +2,7 @@ let swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 2500,
+    delay: 4500,
     disableOnInteraction: false,
   },
   pagination: {
@@ -13,4 +13,34 @@ let swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+// sub-커뮤니티
+// 1sec 탭메뉴
+const roomBtns = document.querySelectorAll(".room_on_off > li");
+const roomConts = document.querySelectorAll(".diceroom_con > div");
+
+roomBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    roomBtns.forEach((otherBtn) => {
+      otherBtn.classList.remove("active");
+    });
+    roomConts.forEach((othercont) => {
+      othercont.classList.remove("active");
+    });
+    roomBtns[index].classList.add("active");
+    roomConts[index].classList.add("active");
+  });
+});
+
+//sec2 - 슬라이드
+let swiper2 = new Swiper(".post_wrap", {
+  // spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
 });
